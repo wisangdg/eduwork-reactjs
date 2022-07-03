95c37771dd887a202a3b987d2d20b67eb1334f3d
Wisang Drillian Geni <gwisang45@gmail.com>
2022-07-04 02:46:55 +0700

    tambahh

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ const App = () => {
   <div className = "App" >
     <header className = "App-header" >
     <img src = {logo}className = "App-logo" alt = "logo" / >
-    <p>Halo </p> 
+    <p >Halo </p> 
     <Button />
-    </header> </div>
+    <a className = "App-link"
+    href = "https://reactjs.org"
+    target = "_blank"
+    rel = "noopener noreferrer" >Learn React </a> </header> </div>
   );
 }
 
